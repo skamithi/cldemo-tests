dcada44dbe40babc7a40e48da7e279b94aa59879
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2014-10-28 07:48:45 -0700

    Drive the '2s' tests from data

--- spec/2s/interfaces_spec.rb
+++ spec/2s/interfaces_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
+require 'json'
+
+node_data = JSON.parse(File.read(File.expand_path('../data/nodes.json',__FILE__)))
+
+# Select the data that's specific to this node
+target_data = node_data[target]
 
 # ospf unnumbered has same IP on a bunch of devices
 for intname in ["lo","swp1","swp2","swp3","swp4"]
   describe interface(intname) do
-    it { should have_ipv4_address("10.2.1.1/32") }
+    it { should have_ipv4_address("#{target_data['local_addr']}/32") }
   end
 end
 
 describe interface('br0') do
-  it { should have_ipv4_address("10.4.1.1/25") }
+  it { should have_ipv4_address("#{target_data['br0_addr']}/25") }
 end
 
 describe interface('br1') do
-  it { should have_ipv4_address("10.4.1.129/25") }
+  it { should have_ipv4_address("#{target_data['br1_addr']}/25") }
 end
--- spec/2s/resolver_spec.rb
+++ spec/2s/resolver_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
-for intname in ["lo","swp1","swp2","swp3","swp4"]
-  describe interface(intname) do
-    it { should have_ipv4_address("10.2.1.1/32") }
-  end
-end
-
 for hostname in ["leaf1","leaf2","wbench"]
   describe host(hostname) do
     it { should be_resolvable }
