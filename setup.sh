3aff7a7a7542b9a3569d0ed6093f11b8db4cfcae
Kristian Van Der Vliet <vanders@liqwyd.com>
2014-11-11 07:58:55 -0800

    Add a simple setup script
    Until I get Ruby 2.1 installed by default in the workbench VM, this script can
    be used to install Ruby 2.1 & dependencies for running these Serverspec tests.

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+then
+  echo "Please run this script as root"
+  exit 1
+fi
+
+# Add the Brightbox Ruby repo
+apt-key adv --keyserver keyserver.ubuntu.com --recv C3173AA6
+
+cat > /etc/apt/sources.list.d/brightbox_ruby_stable.list << _EOF
+deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu precise main 
+deb-src http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu precise main
+_EOF
+
+# Install Ruby 2.1 from Brightbox and set up for Serverspec
+apt-get update -y
+apt-get install -y ruby2.1 rubygems
+apt-get autoremove -y
+gem install bundler
+bundle install
