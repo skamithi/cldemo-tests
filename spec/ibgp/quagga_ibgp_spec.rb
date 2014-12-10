2e2f60aef4845d84caf2e47cf845b0d77d1de936
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2014-12-10 15:07:19 +0000

    Add iBGP specific Quagga tests
    Add some tests for BGP neighbors

---
+++ spec/ibgp/quagga_ibgp_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'json'
+
+node_data = JSON.parse(File.read(File.expand_path('../data/quagga.json',__FILE__)))
+
+# Select the data that's specific to this node
+target_data = node_data[target]
+
+# Select the data that's specific to this node for the given topology
+neighbors = target_data[topology]['neighbors']
+
+# neighbors
+describe command("cl-bgp summary") do
+  for neighbor in neighbors do
+    its(:stdout) { should match /#{neighbor} / }
+  end
+end
