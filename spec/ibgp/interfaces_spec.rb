2f3b4df127d4a1cfb06e149c48101d1874480c91
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2014-12-10 12:43:47 +0000

    Start iBGP tests
    Split the basic Quagga tests out from the OSPF unnumbered tests.
    Start the iBGP tests (just test the interfaces; copy from OSPF unnumbered
    tests for now)

---
+++ spec/ibgp/interfaces_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'json'
+
+node_data = JSON.parse(File.read(File.expand_path('../data/interfaces.json',__FILE__)))
+
+# Select the data that's specific to this node
+target_data = node_data['config'][target]
+
+# ospf unnumbered has same IP on a bunch of devices
+for intname in node_data['interfaces'][topology] 
+  describe interface(intname) do
+    it { should have_ipv4_address("#{target_data['local_addr']}/32") }
+  end
+end
+
+if leaf?
+  describe interface('br0') do
+    it { should have_ipv4_address("#{target_data['br0_addr']}/25") }
+  end
+
+  describe interface('br1') do
+    it { should have_ipv4_address("#{target_data['br1_addr']}/25") }
+  end
+end
