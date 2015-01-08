6d88fb6be1ea83397804839505d320cc05c04151
Leslie Carr <geekgirl@gmail.com>
2015-01-08 12:36:26 -0800

    clagd service test

--- spec/clag/clag_spec.rb
+++ spec/clag/clag_spec.rb
@@ -4,6 +4,10 @@ describe file('/etc/default/clagd') do
     it { should be_file }
 end
 
+describe service('clagd') do
+    it { should be_running }
+end
+
 describe command('clagctl') do
     its(:stdout) { should match /The peer is alive/ }
 end
