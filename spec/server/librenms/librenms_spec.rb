bdd06e9b8f5c9f8feb212b33d30850f6ed932d51
Leslie Carr <geekgirl@gmail.com>
2015-01-16 12:38:37 -0800

    librenms specific tests

--- spec/server/librenms/librenms_spec.rb
+++ spec/server/librenms/librenms_spec.rb
@@ -1,1 +1,38 @@
 require 'spec_helper'
+
+for pkg in ["apache2","libapache2-mod-php","php5-cli","php5-gd","php5-json","php5-mcrypt","php5-mysql","php5-snmp","php-net-ipv4","php-net-ipv6","php-pear","fping","git","graphviz","imagemagick","ipmitool","mtr-tiny","nmap","python-mysqldb","rrdtool","snmp-mibs-downloader","whois"]
+  describe package(pkg) do
+    it { should be_running }
+  end
+end
+
+describe service("apache2") do
+    it { should be_running }
+end
+
+describe port(80) do
+    it { should be_listening.with('tcp6') }
+end
+
+describe file('/var/www/librenms/config.php') do
+    it { should be_file }
+end
+#double check the run once happened
+describe file('/tmp/puppet_once_lock') do
+    it { should be_file }
+end
+
+describe file('/var/www/rrd') do
+    it { should be_directory}
+end
+
+for symlink in ["/usr/local/sbin/librenms-syslog","/etc/cron.d/librenms","/etc/apache2/sites-enabled/librenms.conf"]
+  describe file(symlink) do
+    it { should be_symlink }
+  end
+end
+
+describe user 'librenms' do
+    it { should exist }
+end
+
