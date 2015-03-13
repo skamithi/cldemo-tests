04f060c6c8976ce18cc8995d60752a060d835300
Leslie Carr <geekgirl@gmail.com>
2015-03-13 15:20:22 -0700

    changing vmware test data

--- spec/switch/vmware/interfaces_spec.rb
+++ spec/switch/vmware/interfaces_spec.rb
@@ -5,6 +5,8 @@ node_data = JSON.parse(File.read(File.expand_path('../data/interfaces.json', __F
 
 # Select the data that's specific to this node
 target_data = node_data['config'][target]
+bridges = target_data['bridges']
+bonds = target_data['bonds']
 
 # ospf unnumbered has same IP on a bunch of devices
 target_data['interfaces'].each do |intf|
@@ -14,15 +16,27 @@ target_data['interfaces'].each do |intf|
 end
 
 # Test bonds exist
-target_data['bonds'].each do |bnd|
-  describe bond(bnd) do
+bonds.each do |bnd|
+  describe bond(bnd['name']) do
     it { should exist }
+  end
+
+  bnd['interfaces'].each do |intf|
+    describe bond(bnd['name']) do
+      it { should have_interface(intf) }
+    end
   end
 end
 
 # Test bridges exist
-target_data['bridges'].each do |brdge|
-  describe bridge(brdge) do
+bridges.each do |brdg|
+  describe bridge(brdg['name']) do
     it { should exist }
+  end
+
+  brdg['interfaces'].each do |intf|
+    describe bridge(brdg['name']) do
+      it { should have_interface(intf) }
+    end
   end
 end
