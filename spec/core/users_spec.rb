cc6ca48a2c14a36109716ebf1f3185a58f596c97
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-05-05 16:55:04 +0100

    Remove tests for 'rocket' & 'turtle' users

--- spec/core/users_spec.rb
+++ spec/core/users_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
-%w(cumulus rocket turtle).each do |username|
-  describe user(username) do
-    it { should exist }
-    it { should belong_to_group 'sudo' }
-  end
+describe user('cumulus') do
+  it { should exist }
+  it { should belong_to_group 'sudo' }
 end
